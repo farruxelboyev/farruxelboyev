1b798633f913eafeec4d9870b93cd6fa4cea6216
Farrux Elboyev <elboyevfarrux670@gmail.com>
2024-12-13 17:11:38 +0500

    vazifa c#
    10 lik sanoq sistemasidan 2 lik va 8 likka o'tkazish

---
+++ 10--8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("10-lik son: ");
+        int decimalNumber = int.Parse(Console.ReadLine());
+
+        // 10-likdan 8-lik sanoq sistemasiga o'tkazish
+        string octalNumber = Convert.ToString(decimalNumber, 8);
+
+        Console.WriteLine("8-lik son: " + octalNumber);
+    }
+}
